2dfed6f69e9dc4162ae5f26ef495a71f5e3840d5
Rainier <24210240282@m.fudan.edu.cn>
2026-04-22 19:16:19 +0800

    Update qwen2_7b_instruction.sh

--- WRL/verl/examples/qwen2_7b_instruction.sh
+++ WRL/verl/examples/qwen2_7b_instruction.sh
@@ -2,14 +2,8 @@ set -x
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-# ray stop
-# ray start --head --num-gpus 8
 
-# MODEL_PATH=/nas/shared/kilab/hf-hub/cold_ckpt
-#MODEL_PATH=/oss/rqy/checkpoints/if_all_dp_nc/qwen2_5_7b/actor/huggingface
-
-#MODEL_PATH=/nas/shared/kilab/hf-hub/DeepScaleR-1.5B-Preview
-MODEL_PATH=/mnt/shared-storage-user/evobox-share/hf-hub/models--Qwen2.5-7B-Instruct/snapshots/a09a35458c702b33eeacc393d103063234e8bc28
+MODEL_PATH=models--Qwen2.5-7B-Instruct
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
